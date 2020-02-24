c960b75c3cc0f98bd52f60711855db4dcd02a410
Max K <kuhmichel.max@fh-swf.de>
2020-02-24 14:59:00 +0100

    TF_06 Tested some different options with the ace editor. Most basic and uncomplete tagging(without backend)

---
+++ src/js/react/components/AnnotationPicker.js
@@ -0,0 +1,45 @@
+import React, {Component} from "react";
+import Modal from "react-bootstrap/Modal";
+import PropTypes from "prop-types";
+import ServerNotAvailableInfo from "./ServerNotAvailableInfo";
+import ListGroup from "react-bootstrap/ListGroup";
+class AnnotationPicker extends Component {
+
+    constructor(props) {
+        super(props);
+        this._renderAnnotationsTable = this._renderAnnotationsTable.bind(this)
+    }
+
+    render() {
+        return (
+            <Modal show={this.props.textSelected} onHide={() => {
+
+            }}>
+                <Modal.Header>
+                    <Modal.Title>Pick the correct Annotation!</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    {this._renderAnnotationsTable()}
+                </Modal.Body>
+
+            </Modal>
+        );
+    }
+
+    _renderAnnotationsTable() {
+        return this.props.annotations.map((annotation, idx) => {
+            return   <ListGroup horizontal={"sm"} key={idx}>
+                <ListGroup.Item action onClick={()=>{this.props.onPicked(annotation.a_id)}} sytle={{backgroundColor: annotation.color}}>{annotation.name}</ListGroup.Item>
+            </ListGroup>
+
+        })
+    };
+}
+
+ServerNotAvailableInfo.propTypes = {
+    textSelected: PropTypes.bool,
+    annotations: PropTypes.array,
+    onPicked: PropTypes.func
+};
+
+export default AnnotationPicker
--- src/js/react/views/editor/Editor.js
+++ src/js/react/views/editor/Editor.js
@@ -1,11 +1,32 @@
 import React, {Component} from "react";
+import {Redirect, Route, withRouter} from "react-router-dom";
 import AnnotationEditor from "../../components/AnnotationEditor";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import DropdownItem from "react-bootstrap/DropdownItem";
+import FetchPending from "../../components/FetchPending";
+import fetchStatusType from "../../../redux/actions/FetchStatusTypes";
+
 
 class Configuration extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    // componentDidMount() {
+    //     this.props.fetchDocument();
+    // }
 
     render() {
         return (
             <React.Fragment>
+                <div className="editorNav">
+                    <div>currentCorpus: bla</div>
+                    <div>currentDocument: bla</div>
+                    <div>prev, next</div>
+
+                </div>
+
                 <AnnotationEditor />
             </React.Fragment>
         );
