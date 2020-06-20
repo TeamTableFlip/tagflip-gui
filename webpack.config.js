23a27fd9137bbcce32d1435adc7559d812f3654c
Christian Gawron <gawron.christian@fh-swf.de>
2020-06-20 09:25:05 +0200

    fix: Pack CSS in a .css file to improve compability with CSP headers fix: add prefix for application path to allow hosting on a relative url

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
+const Webpack = require("webpack");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const ASSET_PATH = process.env.ASSET_PATH || '/';
+
 module.exports = {
     output: {
-        publicPath: '/'
+        publicPath: ASSET_PATH
     },
     devServer: {
         historyApiFallback: true,
@@ -26,20 +31,33 @@ module.exports = {
             {
                 test: /\.(sass|scss|css)$/i,
                 use: [
+                    //'file-loader',
                     // Creates `style` nodes from JS strings
-                    'style-loader',
+                    //{ loader: 'style-loader', options: { injectType: 'linkTag' } },
+                    MiniCssExtractPlugin.loader,
                     // Translates CSS into CommonJS
                     'css-loader',
                     // Compiles Sass to CSS
                     'sass-loader',
+                    //'extract-loader',
                 ],
             }
         ]
     },
     plugins: [
+        new Webpack.DefinePlugin({
+            'process.env.ASSET_PATH': JSON.stringify(ASSET_PATH),
+            BASENAME: JSON.stringify(ASSET_PATH)
+        }),
         new HtmlWebPackPlugin({
             template: "./src/index.html",
             filename: "./index.html"
-        })
+        }),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: '[name].css',
+            chunkFilename: '[id].css',
+        }),
     ]
 };
